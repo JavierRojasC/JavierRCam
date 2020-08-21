fc9d0be22a2d808198578f07e81b81fd6af62794
JavierRojasC <62660573+JavierRojasC@users.noreply.github.com>
2020-08-21 16:29:15 -0500

    Add files via upload

--- Scrap.DATA.ec.JRojas.R
+++ Scrap.DATA.ec.JRojas.R
@@ -1,13 +1,15 @@
 DATA.EC.JRojas <- function(){
   require(rvest)
-url <- "https://news.google.com/covid19/map?hl=es-419&gl=US&ceid=US%3Aes-419&mid=%2Fm%2F02k1b"
-READCOVID = read_html(url)
+  url <- "https://www.coronavirusecuador.com/datos-provinciales/"
+  READCOVID = read_html(url)
 tabla= READCOVID %>%
   rvest::html_nodes("table")
 tabla1 <- html_table(tabla[1], fill = TRUE, header = T, dec = ",")
 Table.scrap <- as.data.frame(tabla1)
-Table.scrap$Confirmados <- sub(",", "", Table.scrap$Confirmados, fixed = TRUE)
-Table.scrap$Confirmados <- as.numeric(Table.scrap$Confirmados)
+#Table.scrap$Confirmados <- sub(",", "", Table.scrap$Confirmados, fixed = TRUE)
+Table.scrap$CONFIRMADOS <- as.numeric(Table.scrap$CONFIRMADOS)
+Table.scrap$FALLECIDOS <- as.numeric(Table.scrap$FALLECIDOS)
+
 return(Table.scrap)}
 
 
@@ -16,6 +18,13 @@ Data.Multiv <- function(){
   Datos.Covid.911 <- read.csv2(url.mult)
   Datos.covid.911 <- Datos.Covid.911[1:24,]
   return(Datos.covid.911)
+}
+
+Data.Categ <- function(){
+url.cat <- 'https://raw.githubusercontent.com/JavierRojasC/JavierRCam/master/categorCovid19.csv' 
+Datos.Categ <- read.csv2(url.cat)
+print('ADD')
+  return(Datos.Categ)
 }
 
 #source('https://raw.githubusercontent.com/JavierRojasC/JavierRCam/master/Scrap.DATA.ec.JRojas.R')
